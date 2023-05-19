dccfa7a2aa0922c4dbeacebf93d2675347d095cb
aymen2782 <aymen.sellaouti@gmail.com>
2023-05-19 16:45:27 +0100

    exo loginForm fin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { ColorComponent } from './components/color/color.component';
 import { MiniWordComponent } from './directives/mini-word/mini-word.component';
 import { CvInfosComponent } from './cv/cv-infos/cv-infos.component';
 import { MES_ROUTES } from './config/routes.const';
+import { LoginComponent } from './auth/login/login.component';
 // /word
 const routes: Routes = [
   { path: '', component: FirstComponent },
   { path: MES_ROUTES.cv, component: CvComponent },
   { path: `${MES_ROUTES.cv}/:id`, component: CvInfosComponent },
+  { path: `${MES_ROUTES.login}`, component: LoginComponent },
   { path: 'color/:defaultColor', component: ColorComponent },
   { path: 'word', component: MiniWordComponent },
   // { path: ':quelqueChose', component: SecondComponent },
